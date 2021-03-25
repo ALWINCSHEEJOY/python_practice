3ea5d60a34f3713daa340739831e0f7b417c7a68
ALWIN_C <46865870+ALWINCSHEEJOY@users.noreply.github.com>
2021-03-25 22:49:10 +0530

    question based on using pi

---
+++ pi.py
@@ -0,0 +1,16 @@
+n=int(input("enter no:\n"))
+x=n
+a=n
+s=0
+j=str(x)
+l=len(j)
+while(x>0):
+    x=n%10
+    s+=x**l
+    n=n//10
+if(s==a):
+    print("\n amg")
+else:
+    print("\n n")
+        
+
